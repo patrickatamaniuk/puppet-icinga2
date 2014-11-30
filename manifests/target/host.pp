d698f4fc31518fc0dfbdb840c09ec1192c9ed1f4
Patrick Atamaniuk <patrick.atamaniuk@wibas.de>
2014-11-30 13:16:49 +0100

    default to ipaddress for monitored target

--- manifests/target/host.pp
+++ manifests/target/host.pp
@@ -8,21 +8,36 @@ class icinga2::target::host(
   # enable a set of default checks for this target
   #
   $enable_nrpe_generic_checks = true,
+
+  #
+  # ipv4_address to monitor, defaults to $::ipaddress
+  #  possible values: 'eth0' 'eth1' any ip '1.2.3.4' or a fqdn
+  #
+  $address = undef,
+
   #
   # define vars in host.conf for this target to specify manual configurations
   #
   $extra_vars = {'sla'=>'8x5'},
+
 ) {
   include "icinga2::target"
 
   validate_hash($extra_vars)
+
+  $ipv4_address = $address ? {
+    undef   => $::ipaddress,
+    'eth0'  => $::ipaddress_eth0,
+    'eth1'  => $::ipaddress_eth1,
+    default => $address
+  }
 
   notice("Exporting icinga2::object::host ${fqdn}")
   @@icinga2::object::host { $::fqdn:
     display_name     => $::fqdn,
     target_dir       => "${icinga2::target::customconfigdir}/hosts",
     target_file_name => "${::fqdn}.conf",
-    ipv4_address     => $::fqdn,
+    ipv4_address     => $ipv4_address,
     vars             => merge($extra_vars, {
       os => $operatingsystem,
       kernel => $kernel,
