777b60a5fd08676fcaac7b11892f095af6466e02
Patrick Atamaniuk <patrick.atamaniuk@wibas.de>
2014-12-05 10:27:17 +0100

    fix quoting of ally services, fix applies for commands per host

--- manifests/target/check_command.pp
+++ manifests/target/check_command.pp
@@ -24,7 +24,7 @@ define icinga2::target::check_command (
   include icinga2::target
   include icinga2::target::host
 
-  $icinga_svc = "${command}"
+  $icinga_svc = "${name}"
   exported_vars::set { "icinga_servicevar_${icinga_svc}":
     value => $icinga_svc
   }
@@ -34,8 +34,8 @@ define icinga2::target::check_command (
   @@icinga2::server::stdservices::apply_check_command_service { "${icinga_svc}_${::fqdn}":
     command       => $command,
     vars          => $parameter,
-    display_name  => capitalize($command),
-    object_servicename => capitalize($command), # use a common object for every target
+    display_name  => capitalize($name),
+    object_servicename => capitalize($name), # use a common object for every target
     assign_where  => "host.vars.${icinga_svc} == true",
     tag           => "icinga2_check_${icinga2::target::magic_tag}",
   }
